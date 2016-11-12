36b2ecd785442e426feade7aca02e21300f8193d
q2012 <vlad9696@ukr.net>
2016-11-12 18:32:38 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -6,10 +6,39 @@ var addr = env.NODE_IP || 'localhost';
 
 var app = express();
 
+var mute,mono,bass;
+
+var freq;
+
+var volume;
+
+
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+    
+    var resp = "";
+    if(req.query.goal)
+    {
+        mute = req.query.mute;
+        bass = req.query.bass;
+        mono = req.query.mono;
+
+        freq = req.query.freq;
+
+        vol = req.query.vol;
+        
+        resp+="mute: "+mute+"\nmono: "+mono+"\nbass: "+bass+"\nfrequence: "+freq+"\nvolume: "+vol
+    }
+    else
+    {
+        resp+=mute+"-"+mono+"-"+bass+"-"+freq+"-"+vol;
+    }
+    
+    res.send(resp);
+    
+    
 });
 
 app.listen(port, addr, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Example app listening on '+addr+':'+port+'!');
 });
