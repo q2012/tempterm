c4b16e01deb3937118a95f38234a9814b96548f5
q2012 <vlad9696@ukr.net>
2016-11-13 11:50:16 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -26,13 +26,8 @@ app.get('/', function (req, res) {
         freq = req.query.freq?req.query.freq:freq;
 
         vol = req.query.vol?req.query.vol:vol;
-        
-        resp+="mute: "+mute+"\nmono: "+mono+"\nbass: "+bass+"\nfrequence: "+freq+"\nvolume: "+vol
     }
-    else
-    {
         resp+=mute+"-"+mono+"-"+bass+"-"+freq+"-"+vol;
-    }
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
